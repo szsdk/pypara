895d3c33b1a7427c1572563801927fbac4573a18
szsdk <shen_zhou@u.nus.edu>
2022-04-07 09:47:07 +0200

    Black the project

--- examples/example3.py
+++ examples/example3.py
@@ -6,17 +6,19 @@ from itertools import repeat
 
 lock = multiprocessing.Lock()
 
+
 @click.command()
 @click.option("--lock", is_flag=True)
 def generator(lock):
     return zip(range(4), "abcd", repeat(lock))
 
+
 def run(a, b, l):
     if l:
         lock.acquire()
         time.sleep(0.5)
-        print(a,b)
+        print(a, b)
         lock.release()
     else:
         time.sleep(0.5)
-        print(a,b)
+        print(a, b)
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name             = 'pypara',
-    version          = '0.1',
-    author           = 'szsdk',
-    scripts=['pypara', "pyr"],
-    install_requires = ['click', "more_itertools", "toolz"],
+    name="pypara",
+    version="0.1",
+    author="szsdk",
+    scripts=["pypara", "pyr"],
+    install_requires=["click", "more_itertools", "toolz"],
 )
